a7eac95a2a3d778024304f201180ccc22787c992
Aswin pp <aswin@techno.academy>
2024-08-19 16:43:03 +0530

    loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './Components/Pages/Home/Home';
 import Aboutus from './Components/Pages/Aboutus/Aboutus';
 import Contact from './Components/Pages/Contact/Contact';
@@ -8,60 +8,51 @@ import Menu from './Components/Pages/Menu/Menu';
 import { useEffect, useState } from 'react';
 import Loader from './Components/ReusableComponent/Loader/Loader';
 
-function App() {
+function AppContent() {
   const [loading, setLoading] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
-    const handleStart = () => {
-      setLoading(true);
-    };
+    if (location.pathname === '/') {
+      // Home page: Loader works for 3 seconds
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    } else {
+      // Other pages: Loader depends on network speed
+      const startLoading = () => setLoading(true);
+      const endLoading = () => setLoading(false);
 
-    const handleEnd = () => {
-      setLoading(false);
-    };
+      window.addEventListener('beforeunload', startLoading);
+      window.addEventListener('load', endLoading);
 
-    const startLoading = () => {
-      handleStart();
-    };
+      return () => {
+        window.removeEventListener('beforeunload', startLoading);
+        window.removeEventListener('load', endLoading);
+      };
+    }
+  }, [location.pathname]);
 
-    const endLoading = () => {
-      handleEnd();
-    };
+  return loading ? (
+    <Loader />
+  ) : (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/aboutus' element={<Aboutus />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/gallery' element={<Gallery />} />
+      <Route path='/news' element={<News />} />
+      <Route path='/menu' element={<Menu />} />
+    </Routes>
+  );
+}
 
-    window.addEventListener('beforeunload', startLoading);
-    window.addEventListener('load', endLoading);
-
-    return () => {
-      window.removeEventListener('beforeunload', startLoading);
-      window.removeEventListener('load', endLoading);
-    };
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+function App() {
   return (
-   <>
-     {loading ? (
-        <Loader />
-      ) : (
-      <BrowserRouter>          
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/aboutus' element={<Aboutus />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/gallery' element={<Gallery/>} />
-            <Route path='/news' element={<News />} />
-            <Route path='/menu' element={<Menu />} />
-            </Routes>
-      </BrowserRouter>
-        )}
-   </>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   );
 }
 
